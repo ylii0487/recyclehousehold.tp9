91d0256ad14082beff37975cd80fafd1ebfcc6b8
ylii0487 <ylii0487@student.monash.edu>
2023-03-10 00:19:47 +1100

    build webpage structure

--- OnboardingProject/controller.py
+++ OnboardingProject/controller.py
@@ -1,13 +1,42 @@
 from flask import Flask, render_template
 import model
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates", static_folder='static')
 
 
 @app.route('/')
+@app.route('/home')
 def index():  # put application's code here
-    return model.index_page()
-    # return render_template('HomePage.html')
+    return model.home_page()
+
+
+@app.route('/Classification')
+def classification():
+    return model.classification_page()
+
+
+@app.route('/WasteTrends')
+def trends():
+    return model.trend_page()
+
+
+@app.route('/Event')
+def event():
+    return model.event_page()
+
+
+@app.route('/Feedback')
+def feedback():
+    return model.feedback_page()
+
+@app.route('/FAQ')
+def faq():
+    return model.faq_page()
+
+@app.route('/AboutUs')
+def about():
+    return model.about_page()
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- OnboardingProject/model.py
+++ OnboardingProject/model.py
@@ -3,5 +3,27 @@ import view
 page_view = view.View()
 
 
-def index_page():
+def home_page():
     return page_view("HomePage")
+
+
+def classification_page():
+    return page_view("Classification")
+
+def trend_page():
+    return page_view("WasteTrends")
+
+def event_page():
+    return page_view("Event")
+
+
+def feedback_page():
+    return page_view("Feedback")
+
+
+def faq_page():
+    return page_view("FAQ")
+
+
+def about_page():
+    return page_view("AboutUs")
